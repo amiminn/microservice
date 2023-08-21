f62ab29f77d8a9505e3ef915363ddf2472662a89
amiminn <tholabul.amin@gmail.com>
2023-08-21 23:01:14 +0700

    change homepage

--- resources/js/vue-views/main/index.vue
+++ resources/js/vue-views/main/index.vue
@@ -3,53 +3,77 @@
         <nav
             class="fixed w-full h-14 flex items-center justify-between px-7 backdrop-blur bg-white/10 text-white"
         >
-            <div class="text-3xl">
+            <div class="text-3xl cursor-pointer">
                 🪸
                 <span class="font-bold text-2xl sm:lg:text-3xl">@amiminn</span>
             </div>
             <div>
-                <router-link
-                    to="/login"
-                    class="px-6 py-1 rounded-full text-white border-2 border-white hover:text-gray-900 hover:bg-white hover:font-bold"
-                    >Login</router-link
-                >
+                <vue-feather
+                    @click="menu = !menu"
+                    size="30"
+                    type="align-right"
+                    class="cursor-pointer"
+                ></vue-feather>
             </div>
         </nav>
-        <div
-            class="pt-14 min-h-screen bg-gray-900 text-white text-center flex items-center"
-        >
-            <div class="container mx-auto grid gap-7">
-                <span class="text-4xl sm:lg:text-7xl font-extrabold tracking"
-                    >🐠 We Help
-                    <span
-                        class="text-white bg-sky-900 px-4 border-2 border-white rounded-xl"
-                        >Bussiness</span
-                    >
-                    with Digital Issue</span
-                >
-                <div class="text-lg">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Ipsum excepturi quasi dolorem ad illum quas delectus
-                    eligendi nobis aliquid reiciendis.
-                </div>
-                <div class="flex gap-3 justify-center">
-                    <button
-                        class="px-7 py-2 bg-gray-900 hover:scale-150 transition rounded-full border-2 border-white"
-                    >
-                        Get start
-                    </button>
-                    <button
-                        class="px-7 py-2 bg-gray-900 hover:scale-150 transition rounded-full border-2 border-white"
-                    >
-                        Join now
-                    </button>
+        <div class="pt-14 min-h-screen bg-gray-900 grid justify-items-center">
+            <div class="text-white flex items-center max-w-sm text-center px-3">
+                <div>
+                    <div class="text-xl font-bold mb-5">Rules</div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing
+                        elit. Necessitatibus error qui, harum ipsum quam maiores
+                        asperiores sequi. Maxime atque, quia nulla dolores
+                        tempore, doloremque optio nihil aut neque accusantium
+                        aliquam?
+                    </p>
+                    <div class="mt-9">
+                        <router-link
+                            to="login"
+                            class="rounded-md hover:bg-blue-800 py-2 px-10 border-white border-2 transition"
+                        >
+                            lanjut
+                        </router-link>
+                    </div>
                 </div>
             </div>
         </div>
-        <!-- <div class="min-h-screen pt-14">bag 2</div> -->
     </div>
+    <Transition
+        enter-active-class="animate__animated animate__zoomIn animate__faster"
+        leave-active-class="animate__animated animate__zoomOut animate__faster"
+    >
+        <div
+            v-if="menu"
+            @click="menu = false"
+            class="fixed top-0 left-0 right-0 z-50 w-full overflow-x-hidden overflow-y-auto md:inset-0 min-h-screen backdrop-blur-sm"
+        >
+            <div class="relative w-full min-h-screen grid justify-items-center">
+                <div class="flex items-center text-center px-3">
+                    <div
+                        class="h-64 w-64 bg-gray-900 rounded-md border-2 border-white py-3 px-4 grid content-center text-white font-bold"
+                    >
+                        <div>
+                            <div
+                                class="text-blue-500 rounded-md border-2 border-white py-1 scale-105 transition my-1"
+                            >
+                                Home
+                            </div>
+                            <div class="my-1">About</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </Transition>
 </template>
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            menu: false,
+        };
+    },
+};
 </script>
 <style lang=""></style>
